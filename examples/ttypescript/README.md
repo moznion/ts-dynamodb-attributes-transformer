288e1f783fdb5576512c21ceeb69f9c6bf8b81b9
moznion <moznion@mail.moznion.net>
2022-10-15 19:18:02 -0700

    Add an example project that uses ttypescript
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ examples/ttypescript/README.md
@@ -0,0 +1,10 @@
+# Using [ttypescript](https://github.com/cevek/ttypescript) example
+
+## How to run
+
+```
+npm run run
+```
+
+Please see also [package.json](./package.json) and [tsconfig.json](./tsconfig.json).
+
---
+++ examples/ttypescript/index.ts
@@ -0,0 +1,19 @@
+import { dynamodbRecord } from '../../index';
+import { AttributeValue } from '@aws-sdk/client-dynamodb';
+
+interface User {
+  readonly id: number;
+  readonly name: string;
+  readonly tags: Map<string, string>;
+}
+
+const record: Record<string, AttributeValue> = dynamodbRecord<User>({
+  id: 12345,
+  name: 'John Doe',
+  tags: new Map<string, string>([
+    ['foo', 'bar'],
+    ['buz', 'qux'],
+  ]),
+});
+
+console.log(JSON.stringify(record));
